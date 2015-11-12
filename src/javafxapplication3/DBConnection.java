600877968a1b23626cdc91de50468d39b1860cc8
thana <thana@192.168.1.6>
2015-11-12 15:55:49 +0800

    Add Libraries

--- src/javafxapplication3/DBConnection.java
+++ src/javafxapplication3/DBConnection.java
@@ -5,10 +5,37 @@
  */
 package javafxapplication3;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author thana
  */
 public class DBConnection {
-    
+    private static Connection conn;  
+    private static String url = "jdbc:mysql://infodynamics.zapto.org/iphp";  
+    private static String user = "root";//Username of database  
+    private static String pass = "";//Password of database  
+    public static Connection connect() throws SQLException{  
+   
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();  
+        }catch(ClassNotFoundException cnfe){  
+            System.err.println("Error: "+cnfe.getMessage());  
+        }catch(InstantiationException ie){  
+            System.err.println("Error: "+ie.getMessage());  
+        }catch(IllegalAccessException iae){  
+            System.err.println("Error: "+iae.getMessage());  
+        }  
+        conn = DriverManager.getConnection(url,user,pass);  
+        return conn;  
+    }
+    public static Connection getConnection() throws SQLException, ClassNotFoundException{  
+     if(conn !=null && !conn.isClosed())  
+       return conn;  
+     connect();  
+     return conn;  
+    }  
 }
